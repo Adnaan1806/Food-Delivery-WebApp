8a93851848b2d2e606bcc4904f080a162ad90a88
Adnaan1806 <CODSE221F-067@student.nibm.lk>
2024-05-24 00:43:46 +0530

    Admin : Add Item Implementation Successfull Added Toastify Message

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -18,7 +18,7 @@ const addFood = async (req,res) => {
    })
    try{
     await food.save();
-    res.json({success:true,message:"Food Added"})
+    res.json({success:true,message:"Food Added Successfully"})
    }
    catch(error){
       console.log(error)
