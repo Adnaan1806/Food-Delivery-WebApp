6bc819c7f9fc60b9e07d9ca021ca328f2720da21
Adnaan1806 <CODSE221F-067@student.nibm.lk>
2024-05-23 20:45:15 +0530

    Place order page completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,7 +19,7 @@ const [showLogin,setShowLogin] = useState(false)
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/cart' element={<Cart/>}/>
-        <Route path='/cart' element={<PlaceOrder/>}/>
+        <Route path='/order' element={<PlaceOrder/>}/>
       </Routes>
     </div>
     <Footer/>
--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -21,9 +21,17 @@ const StoreContexProvider = (props)=>{
             setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
     }
 
-    useEffect(()=>{
-       console.log(cartItems);
-    },[cartItems])
+   const getTotalCartAmount = () =>{
+     let totalAmount = 0;
+        for(const item in cartItems){
+           
+            if(cartItems[item] > 0){
+                let itemInfo = food_list.find((product)=>product._id === item);
+                totalAmount += itemInfo.price * cartItems[item];
+            }
+        }
+        return totalAmount;
+   }
 
 
     const contextValue = {
@@ -31,7 +39,8 @@ const StoreContexProvider = (props)=>{
         cartItems,
         setCartItems,
         addToCart,
-        removeFromCart
+        removeFromCart,
+        getTotalCartAmount
     }
 
     return(
